2d38ea6655a6f545668fefbd4e9f0e70428282a7
Sean Stowers <sean@Seans-MacBook-Pro.local>
2013-05-14 19:25:53 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+VetPractice::Application.config.secret_token = '0820f5c9e63873124f80817bd96d47d7615004b9e817cacf7211f280878f80498296ebd1676f85a4c6bf36c401619b7bf569208ec176628ba92edf8f59b52953'
